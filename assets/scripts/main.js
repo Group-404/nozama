78f200af4f007fa7ec182d853eaa5640729da7a6
Lara Parvinsmith <lara.parvinsmith@gmail.com>
2015-08-22 23:52:13 -0400

    adds if statement for displaying sale price, and buttons (non-functioning) for 'show bikes' and 'show accessories'

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,14 +1,26 @@
 'use strict';
 
+// fake database. delete when connect to back-end.
 var data = { products: [
-  {name: "specialized roadbike", category: "bike", price: 400},
-  {name: "schwinn mountainbike", category: "bike", price: 550},
-  {name: "schwinn helmet", category: "accessory", price: 30},
-  {name: "bike gloves", category: "accessory", price: 25}
+  {id: 1, name: "specialized roadbike", category: "bike", price: 400},
+  {id: 2, name: "schwinn mountainbike", category: "bike", price: 550, sale: 499},
+  {id: 3, name: "schwinn helmet", category: "accessory", price: 30},
+  {id: 4, name: "bike gloves", category: "accessory", price: 25, sale: 19}
 ]};
 
 $(document).ready(function(){
 
-$("#products").html(View.productIndexHTML({products: data.products}));
+  // on document ready, show all products
+  $("#products").html(View.productIndexHTML({products: data.products}));
+
+  // when click button show-bikes, show only bikes
+  $("#show-bikes").on('click', function(){
+
+  });
+
+  // when click button accessories, show only accessories
+  $("#show-accessories").on('click', function(){
+
+  });
 
 });
