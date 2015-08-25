10962a6929ddca548b7d4628d2f0f0b200cfb227
jlevinger <joe@joelevinger.com>
2015-08-25 12:07:58 -0400

    Completes Handlebars for Bicycles page

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,4 +1,17 @@
 'use strict';
+// Fake database. Delete when we connect to back-end.
+var data = { products: [
+  {id: 1, category: 'bicycles', name: 'Breezer Downtown 5 Bike', description: 'The Breezer Downtown 5 Bike is the ideal ride for your urban adventures around town, whether it be a long expedition or a simple ride to work.', price: 569, imageURL: 'bike_breezer_lg.jpg', thumbnailURL: 'bike_breezer_sm.jpg'},
+  {id: 2, category: 'bicycles', name: 'Schwinn City 3 Bike 2013 - Womens', description: 'In a classic design with modern lightweight performance, the Schwinn City 3 Bike for women lets you get around town easily and look good doing it.', price: 550, sale: 269.95, imageURL: 'bike_schwinn_lg.jpg', thumbnailURL: 'bike_schwinn_sm.jpg'},
+  {id: 3, category: 'helmets', name: 'Giro Reverb Helmet', model: 'matte gray', price: 55, imageURL: 'helmet_shadow_lg.jpg', thumbnailURL: 'helmet_shadow_sm.jpg'},
+  {id: 4, category: 'helmets', name: 'Giro Reverb Helmet', model: 'Highlight Yellow', price: 55, imageURL: 'helmet_yellow_lg.jpg', thumbnailURL: 'helmet_yellow_sm.jpg'},
+  {id: 5, category: 'helmets', name: 'Giro Reverb Helmet', model: 'Matte White CA Beary', price: 55, imageURL: 'helmet_white_lg.jpg', thumbnailURL: 'helmet_white_sm.jpg'},
+  {id: 6, category: 'helmets', name: 'Giro Reverb Helmet', model: 'Vintage Red', price: 55, imageURL: 'helmet_red_lg.jpg', thumbnailURL: 'helmet_red_sm.jpg'},
+  {id: 7, category: 'locks', name: 'Kryptonite Keeper 12 Standard',  price: 35, sale: 30, imageURL: 'lock_keeper_lg.jpg', thumbnailURL: 'lock_keeper_sm.jpg'},
+  {id: 8, category: 'locks', name: 'Kryptonite Series 2 Mini Ulock - Red',  price: 35, imageURL: 'lock_red_lg.jpg', thumbnailURL: 'lock_red_sm.jpg'},
+  {id: 9, category: 'locks', name: 'Kryptonite Series 2 Mini Ulock - White',  price: 35, imageURL: 'lock_white_lg.jpg', thumbnailURL: 'lock_white_sm.jpg'},
+  {id: 10, category: 'locks', name: 'Kryptonite Series 2 Mini Ulock - Black',  price: 35, imageURL: 'lock_black_lg.jpg', thumbnailURL: 'lock_black_sm.jpg'},
+]};
 
 var server = '//localhost:3000';
 // var server = '[place production server here]';
@@ -9,8 +22,10 @@ var server = '//localhost:3000';
 
 $(document).ready(function() {
 
+  // invokes carousel
   $('#carousel').carousel();
 
+  // initial state of landing page
   $('#carousel').show();
   $('#bikepage').hide();
   $('#accessoriespage').hide();
@@ -22,7 +37,7 @@ $(document).ready(function() {
   // BEGIN: show appropriate page; hide the rest
   //////////////////////////////////////////////
 
-   $('#logo').on('click', function(event) {
+  $('#logo').on('click', function(event) {
     event.preventDefault();
     $('#carousel').show();
     $('#bikepage').hide();
@@ -103,33 +118,27 @@ $(document).ready(function() {
   // END: show appropriate page, hide the rest
   //////////////////////////////////////////////
 
+  //////////////////////////////////////////////
+  // BEGIN: prepare all handlebars objects
+  //////////////////////////////////////////////
+
+  $('.bicycles').on('click', function() {
+    var products = data.products;
+    var bicycles = $.grep(products, function(e) { return e.category === 'bicycles';
+    });
+    $('#bicycleResults').html(View.bicycleIndexHTML({bicycles: bicycles}));
+  });
+
+  //
+  // $("#products").html(View.productIndexHTML({products: bikes}));
+  // });
+
+  //////////////////////////////////////////////
+  // END: prepare all handlebars objects
+  //////////////////////////////////////////////
+
 });
 
 //////////////////////////////////////////////
 // END: document.ready
 //////////////////////////////////////////////
-
-// // fake database. delete when connect to back-end.
-// var data = { products: [
-//   {id: 1, name: "specialized roadbike", category: "bike", price: 400},
-//   {id: 2, name: "schwinn mountainbike", category: "bike", price: 550, sale: 499},
-//   {id: 3, name: "schwinn helmet", category: "accessory", price: 30},
-//   {id: 4, name: "bike gloves", category: "accessory", price: 25, sale: 19}
-// ]};
-
-// $(document).ready(function(){
-
-//   // on document ready, show all products
-//   $("#products").html(View.productIndexHTML({products: data.products}));
-
-//   // when click button show-bikes, show only bikes
-//   $("#show-bikes").on('click', function(){
-
-//   });
-
-//   // when click button accessories, show only accessories
-//   $("#show-accessories").on('click', function(){
-
-//   });
-
-// });
