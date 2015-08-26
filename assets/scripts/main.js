46536bedc2a1b883569df22a510c787d6c1ddda9
Lara Parvinsmith <lara.parvinsmith@gmail.com>
2015-08-26 14:42:59 -0400

    adds comments where further explanation is needed

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -3,29 +3,13 @@
 var server = '//localhost:3000';
 // var server = '[production server name goes here]';
 
-// Fake database. Delete when we connect to back-end.
-// var data = { products: [
-//   {id: 1, category: 'bicycles', name: 'Breezer Downtown 5 Bike', description: 'The Breezer Downtown 5 Bike is the ideal ride for your urban adventures around town, whether it be a long expedition or a simple ride to work.', price: 569, imageURL: 'bike_breezer_lg.jpg', thumbnailURL: 'bike_breezer_sm.jpg'},
-//   {id: 2, category: 'bicycles', name: 'Schwinn City 3 Bike 2013 - Womens', description: 'In a classic design with modern lightweight performance, the Schwinn City 3 Bike for women lets you get around town easily and look good doing it.', price: 550, sale: 269.95, imageURL: 'bike_schwinn_lg.jpg', thumbnailURL: 'bike_schwinn_sm.jpg'},
-//   {id: 3, category: 'helmets', name: 'Giro Reverb Helmet', model: 'matte gray', price: 55, imageURL: 'helmet_shadow_lg.jpg', thumbnailURL: 'helmet_shadow_sm.jpg'},
-//   {id: 4, category: 'helmets', name: 'Giro Reverb Helmet', model: 'Highlight Yellow', price: 55, imageURL: 'helmet_yellow_lg.jpg', thumbnailURL: 'helmet_yellow_sm.jpg'},
-//   {id: 5, category: 'helmets', name: 'Giro Reverb Helmet', model: 'Matte White CA Beary', price: 55, imageURL: 'helmet_white_lg.jpg', thumbnailURL: 'helmet_white_sm.jpg'},
-//   {id: 6, category: 'helmets', name: 'Giro Reverb Helmet', model: 'Vintage Red', price: 55, imageURL: 'helmet_red_lg.jpg', thumbnailURL: 'helmet_red_sm.jpg'},
-//   {id: 7, category: 'locks', name: 'Kryptonite Keeper 12 Standard',  price: 35, sale: 30, imageURL: 'lock_keeper_lg.jpg', thumbnailURL: 'lock_keeper_sm.jpg'},
-//   {id: 8, category: 'locks', name: 'Kryptonite Series 2 Mini Ulock - Red',  price: 35, imageURL: 'lock_red_lg.jpg', thumbnailURL: 'lock_red_sm.jpg'},
-//   {id: 9, category: 'locks', name: 'Kryptonite Series 2 Mini Ulock - White',  price: 35, imageURL: 'lock_white_lg.jpg', thumbnailURL: 'lock_white_sm.jpg'},
-//   {id: 10, category: 'locks', name: 'Kryptonite Series 2 Mini Ulock - Black',  price: 35, imageURL: 'lock_black_lg.jpg', thumbnailURL: 'lock_black_sm.jpg'},
-// ]};
-
-var server = '//localhost:3000';
-// var server = '[place production server here]';
-
 //////////////////////////////////////////////
 // BEGIN: document.ready
 //////////////////////////////////////////////
 
 $(document).ready(function() {
 
+  // WAT
   window.Main = window.Main || {};
 
   // invokes carousel
@@ -89,6 +73,7 @@ $(document).ready(function() {
     $('#cartpage').hide();
   }
 
+  // WAT
   window.Main.classShowClickHandler1 = classShowClickHandler1;
 
   $('.show').on('click', classShowClickHandler1);
@@ -146,6 +131,7 @@ $(document).ready(function() {
     $('#accessoryResults').html(View.accessoryIndexHTML({accessories: accessories}));
   });
 
+  // WAT
   function classShowClickHandler2(event) {
     console.log('show cliked, data is:  ' + data.products);
     var products = data.products;
@@ -160,6 +146,7 @@ $(document).ready(function() {
     $('#productResults').html(View.itemShowHTML({product: product}));
   }
 
+  // WAT
   window.Main.classShowClickHandler2 = classShowClickHandler2;
 
   $('.show').on('click', classShowClickHandler2);
